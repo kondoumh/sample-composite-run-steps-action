38493c7296da0825373c7a459844d032652d6fed
kondoumh <kondoumh@gmail.com>
2020-12-05 09:50:51 +0900

    remove install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-curl -LO https://github.com/roboll/helmfile/releases/download/v0.135.0/helmfile_linux_amd64
-mv helmfile_linux_amd64 helmfile
-chmod +x helmfile
-ls -l
-mkdir $HOME/bin
-cp helmfile $HOME/bin
-echo "$HOME/bin" >> $GITHUB_PATH
