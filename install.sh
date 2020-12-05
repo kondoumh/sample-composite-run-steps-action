ea3cf569acfe851f17b72e167288f0f99c7421ca
kondoumh <kondoumh@gmail.com>
2020-12-05 09:30:08 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ curl -LO https://github.com/roboll/helmfile/releases/download/v0.135.0/helmfile_
 mv helmfile_linux_amd64 helmfile
 chmod +x helmfile
 ls -l
-cp helmfile ~/.local/bin
+mkdir $HOME/bin
+cp helmfile $HOME/bin
+echo ::add-path::$HOME/bin
